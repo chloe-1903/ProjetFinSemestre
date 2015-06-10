2681e14828bce74059aa61d5555a21fa6ae42ce1
Lisa <Lisa Joanno>
2015-06-10 20:25:54 +0200

    fichier de test

--- tstCacheList.c
+++ tstCacheList.c
@@ -5,39 +5,66 @@
 
 int main(int argc, char const *argv[])
 {
-    struct Cache_List *cache_list=Cache_List_Create();
-    bool testEmpty = Cache_List_Is_Empty(cache_list);
+    struct Cache_List *cache_list = Cache_List_Create();
 
-    Cache_List_Print(cache_list);
-    printf(testEmpty ? "Liste vide\n" : "Liste non vide\n");
     struct Cache_Block_Header *headerTest = (struct Cache_Block_Header*)malloc(sizeof(struct Cache_Block_Header));
     headerTest->ibfile = NULL;
     headerTest->ibcache = NULL;
     headerTest->data = NULL; 
-    Cache_List_Append(cache_list, headerTest);
-    testEmpty = Cache_List_Is_Empty(cache_list);
-    Cache_List_Print(cache_list);
-    printf(testEmpty ? "Liste vide\n" : "Liste non vide\n");
-    Cache_List_Append(cache_list, headerTest);
-    Cache_List_Print(cache_list);
     struct Cache_Block_Header *headerTest2 = (struct Cache_Block_Header*)malloc(sizeof(struct Cache_Block_Header));
-    headerTest2->ibfile = NULL;
-    headerTest2->ibcache = NULL;
-    headerTest2->data = NULL; 
+    headerTest->ibfile = NULL;
+    headerTest->ibcache = NULL;
+    headerTest->data = NULL; 
+    
+    Cache_List_Append(cache_list, headerTest);
+    Cache_List_Append(cache_list, headerTest);
+    printf("Avant preprend : \n");
+    Cache_List_Print(cache_list);
+    Cache_List_Prepend(cache_list, headerTest);
+    Cache_List_Prepend(cache_list, headerTest2);
     Cache_List_Append(cache_list, headerTest2);
-    printf("Après le dernier ajout :\n");
+    printf("Après les preprend : \n");
     Cache_List_Print(cache_list);
-    printf("\n\n REMOVE \n");
-    Cache_List_Remove(cache_list, headerTest2);
+    
+    //~ printf("C'est parti pour delete\n");
+    //~ Cache_List_Delete(cache_list);
+    //~ Cache_List_Print(cache_list);
+    
+    
+    printf("C'est parti pour REMOVE FIRST\n");
+    Cache_List_Remove_First(cache_list);
     Cache_List_Print(cache_list);
-    printf("Après remove 1\n");
-    Cache_List_Remove(cache_list, headerTest);
-    Cache_List_Print(cache_list);
-    printf("Après remove 2\n");
-    Cache_List_Remove(cache_list, headerTest);
-    Cache_List_Print(cache_list);
-    printf("Après remove 3\n");
-    free(headerTest);
+    
+    
+    //~ Cache_List_Append(cache_list, headerTest);
+    //~ Cache_List_Print(cache_list);
+    //~ struct Cache_Block_Header *headerTest2 = (struct Cache_Block_Header*)malloc(sizeof(struct Cache_Block_Header));
+    //~ headerTest2->ibfile = NULL;
+    //~ headerTest2->ibcache = NULL;
+    //~ headerTest2->data = NULL; 
+    //~ Cache_List_Append(cache_list, headerTest2);
+    //~ printf("Après le dernier ajout :\n");
+    //~ Cache_List_Print(cache_list);
+    
+    //~ printf("\n\n PREPEND\n");
+    //~ Cache_List_Prepend(cache_list,headerTest2);
+    //~ Cache_List_Print(cache_list);
+    
+    
+    //~ printf("\n\n REMOVE \n");
+    //~ printf("\nAprès remove 1 : \n");
+    //~ Cache_List_Remove(cache_list, headerTest2);
+    //~ Cache_List_Print(cache_list);
+    //~ 
+    //~ printf("\nAprès remove 2 : \n");
+    //~ Cache_List_Remove(cache_list, headerTest);
+    //~ Cache_List_Print(cache_list);
+    //~ 
+    //~ printf("\nAprès remove 3 : \n");
+    //~ Cache_List_Remove(cache_list, headerTest);
+    //~ Cache_List_Print(cache_list);
+    
+    //~ free(headerTest);
 
     return 0;
 }
