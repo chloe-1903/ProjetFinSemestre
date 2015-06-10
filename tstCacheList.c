1bbc45624b15f4189f52ba02e249507c2c7c1978
Nicolas Hory <hory.nicolas@etu.unice.fr>
2015-06-10 10:12:12 +0200

    Test du cache_List

--- tstCacheList.c
+++ tstCacheList.c
@@ -9,11 +9,15 @@ int main(int argc, char const *argv[])
     headerTest->ibfile = NULL;
     headerTest->ibcache = NULL;
     headerTest->data = NULL; 
+    printf("%p\n", cache_list);
     Cache_List_Append(cache_list, headerTest);
+    printf("%p\n", cache_list->next);
+    Cache_List_Append(cache_list, headerTest);
+    printf("%p\n", (cache_list->next)->next);    
     struct Cache_List *tmp;
     while (cache_list->next){
         tmp=cache_list->next;
-        printf("Un parcours");
+        printf("Un parcours\n");
         cache_list=tmp;
     }
     return 0;
