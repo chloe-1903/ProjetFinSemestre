7f96bb14f1d14cd856f656cfba154db301383841
Asmae Fadil <fad.asma.1995@gmail.com>
2015-06-10 15:18:24 +0200

    modif cache.c

--- cache.c
+++ cache.c
@@ -2,26 +2,34 @@
 struct Cache *cache;
 
 struct Cache *Cache_Create(const char *fic, unsigned nblocks, unsigned nrecords,size_t recordsz, unsigned nderef){
+	cache =malloc(sizeof(struct Cache));
+	FILE *file;
+	if( (file = fopen(fic, "r+b")) == NULL)
+	file = fopen(fic, "w+b");
+	cache ->fp=file;
 	cache->file= fic;
 	cache->nblocks= nblocks;
-	cache->nrecords=nrecords;	
+	cache->nrecords=nrecords;
+	cache->blocksz=n*recordsz;
 	cache->ndref= nderef;
 	cache->instrument.n_reads = 0;
     cache->instrument.n_writes = 0; 
 	cache->instrument.n_hits = 0;
-	//pstrategy = Strategy_Create( __paramètres__ );
-	//pfree =Get_Free_Block(__paramètre__);
+	pstrategy = Strategy_Create(*cache);
+	pfree =Get_Free_Block(*cache);
 	
 }
 
-/*int Cache_Sync(struct Cache *pcache){
+int Cache_Sync(struct Cache *pcache){
 	for (int i =0; i<pcache->nblocks; i++){
-		if ((pcache->headers[num_bloc].flags & MODIF) > 0){
+		if ((pcache->headers[i].flags & MODIF) > 0){
+			fseek(char *fic,DADDR(pcache, ibfile),SEEK_SET);
+			fwrite(block->data, pcache->nblocks,pcache->nrecords,fic);
 		}
-		fseek(fic , , );
+		pcache->headers[i].flags &= ~MODIF;
 	}
 
-}*/
+}
 int Cache_Close(struct Cache *pcache){
 	Cache_sync(pcache);
 	close(pcache->fp);
