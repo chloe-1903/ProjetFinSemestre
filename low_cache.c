b44a6dc7a9fee49b2d15b6bd1763a17d54434ff5
Lisa <Lisa Joanno>
2015-06-11 14:20:49 +0200

    tout

--- low_cache.c
+++ low_cache.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 #include "low_cache.h"
 
 struct Cache_Block_Header *Get_Free_Block(struct Cache *pcache){
+    struct Cache_Block_Header *freebloc = NULL;
 	for (int num_bloc = 0; num_bloc < pcache->nblocks; ++num_bloc){ 
-		if (pcache->headers[num_bloc].flags & VALID == 0)
-			return &(pcache->headers[num_bloc]);
+		if ((pcache->headers[num_bloc].flags & VALID) == 0) {
+            freebloc = &(pcache->headers[num_bloc]);
+            Init_Free_Bloc_Flags(freebloc);
+			break;
+        }
 	}
-	return NULL;
+	return freebloc;
+}
+
+
+void Init_Free_Bloc_Flags(struct Cache_Block_Header *header) {
+    header->flags |= VALID;
+    header->flags &= MODIF;
 }
