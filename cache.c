cb8842b4e5a23dbd1323179164b75e33ce761afb
Asmae Fadil <fad.asma.1995@gmail.com>
2015-06-12 06:54:44 +0200

    modif class cache.c

--- cache.c
+++ cache.c
@@ -65,11 +65,15 @@ Cache_Error Cache_Close(struct Cache *pcache){
 	Cache_sync(pcache);
 //fermer ce qu'il faut fermer 
 	fclose(pcache->fp);//ferme le fichier
-	Strategy_Close(pcache->pstrategy);
 //free tout les struct
-	free(pcache);
-	free(pcache->headers);
+	for(int i =0; i<nblocks;i++){
+
+		free(pcache->headers[i].data;
+	}
 	pcache=pcache->headers=NULL;
+	free(pcache->headers);
+	free(pcache);
+	Strategy_Close(pcache->pstrategy);
 	return CACHE_OK;
 }
 
@@ -91,8 +95,27 @@ Cache_Error Cache_Invalidate(struct Cache *pcache){
 Cache_Error Cache_Read(struct Cache *pcache, int irfile, const void *precord){
 	
 }
+int IndexSearch(int ibfile, struct Cache* pcache){
+	for(int i = 0; i < pcache->nblocks; i++){
+		struct Cache_Block_Header block = pcache->headers[i];
+//Si le bloc est valide et l'indice est ce qu'on cherche,
+//retourner la position du bloc dans le cache
+		if((block.flags & VALID) && block.ibfile == ibfile)
+		return i;
+	}
+	return -1;
 
+}
+//a terminer
 Cache_Error Cache_Write(struct Cache *pcache, int irfile, const void *precord){
+	ibfile =irfile/nrecords;
+	int var =IndexSearch( ibfile, pcache);
+	if( var >-1){
+		memcpy(pcache->headers[var].data,precord,recordsz);
+	}else{
+		struct Cache_Block_Header* block =Strategy_Replace_Block(pcache);
+		Cache_Sync(pcache);
+	}
 	
 }
 /**
@@ -100,10 +123,10 @@ Cache_Error Cache_Write(struct Cache *pcache, int irfile, const void *precord){
 * tous les compteurs de la structure courante sont remis à 0 par cette fonction.
 **/
 struct Cache_Instrument *Cache_Get_Instrument(struct Cache *pcache){
-	//Copier champ INSTRUMENT de pcache
+	//Copier le champ INSTRUMENT de pcache
 	struct Cache_Instrument* copy = (struct Cache_Instrument*)malloc( sizeof(struct Cache_Instrument) );
 	memcpy( copy, &pcache->instrument, sizeof(struct Cache_Instrument) );
-	//Met tous à zero
+	//Mettre tout à zero
 	pcache->instrument.n_reads = 0;
     pcache->instrument.n_writes = 0; 
 	pcache->instrument.n_hits = 0;
