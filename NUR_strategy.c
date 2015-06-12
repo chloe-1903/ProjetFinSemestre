90b96e3cd98e042981c272704fac52f34be65e71
chloe-1903 <chloe-1903@live.fr>
2015-06-12 10:59:44 +0200

    NUR propre

--- NUR_strategy.c
+++ NUR_strategy.c
@@ -11,37 +11,24 @@
 #include "low_cache.h"
 #include "time.h"
 
-int n_acces=0; //nombre d'accès depuis le dernier dereferencement
+int n_acces=0; //nombre d'accès (lecture/ecriture) depuis le dernier dereferencement
 
-/*!
- * FIFO : pas grand chose à faire ici. 
- *
- * En fait, nous initialisons le germe
- * (seed) du générateur aléatoire à quelque chose d'éminemment variable, pour
- * éviter d'avoir la même séquence à chque exécution...
- */
+
 void *Strategy_Create(struct Cache *pcache) 
 {
 }
 
-/*!
- * FIFO : Rien à faire ici.
- */
 void Strategy_Close(struct Cache *pcache)
 {
 }
 
-/*!
- * FIFO : Rien à faire ici.
- */
+
 void Strategy_Invalidate(struct Cache *pcache)
 {
 	deref(pcache);
 }
 
-/*! 
- * FIFO : On prend le premier bloc invalide. S'il n'y en a plus, on prend un bloc au hasard.
- */
+
 struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
 {
     struct Cache_Block_Header *pbh;
@@ -53,6 +40,7 @@ struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache)
 	return pbh;
     }
 
+	/*Sinon*/
     for (num_bloc = 0; num_bloc < pcache->nblocks; ++num_bloc)
     {    
 	int i=0;   
@@ -74,51 +62,36 @@ struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache)
 }
 
 /*!
- * RAND : Rien à faire ici.
- */
+ * Augmenter le nombre d'accès depuis le dernier dereferençage puis mettre R à 1 et déréférence si besoin
+ */  
 void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
-{
-	//printf("Read %p \n", pbh);	
+{	
 	n_acces++;
 	deref(pcache);
 	pbh->flags |= REF;
 }  
 
 /*!
- * RAND : Rien à faire ici.
+ * Augmenter le nombre d'accès depuis le dernier dereferençage puis mettre R à 1 et déréférence si besoin
  */  
 void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh)
 {
-	//printf("Write %p \n", pbh);
 	n_acces++;
 	deref(pcache);
-	//mettre M à 1?
 	pbh->flags |= REF;
 } 
 
+/* Déréférence (met tous les R à 0) si le nombre d'accès depuis le dernier déréférençage est superieur à nderef */
 void deref(struct Cache *pcache){
 	int num_bloc;
 	if (n_acces >= pcache->nderef){
-       		//printf("Dans la condition deref\n");
 		n_acces=0;
-		//printf("Nb de blocs: %d" , pcache->nblocks);
 		for ( num_bloc = 0;  num_bloc < pcache->nblocks;  num_bloc++) {
-          		//printf("Parcours bloc %p", pcache->headers[num_bloc]);
 			pcache->headers[num_bloc].flags &= ~REF;
 		}
 		++pcache->instrument.n_deref;
 			
 	}
-	//incrémenter n_deref??
-}
-
-int evaluate_RM(struct Cache_Block_Header *bloc)
-{
-    int rm = 0;
-    if ((bloc->flags & REF) > 0) rm += 2;
-    if ((bloc->flags & MODIF) > 0) rm += 1;
-
-    return rm;
 }
 
 char *Strategy_Name()
